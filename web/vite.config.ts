24529112b5aa732081fdb3220760508e73e423f4
Jayson Zhong <zgchung@outlook.com>
2026-03-05 23:09:39 +0800

    fix: use relative paths for assets

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  build: {
-    target: 'es2015',
-    minify: 'esbuild',
-    sourcemap: false,
-  },
+  base: './',
   plugins: [
     react(),
   ],
